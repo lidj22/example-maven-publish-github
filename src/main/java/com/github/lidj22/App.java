cf4b0c69a96bfd95a3b37ce27c82e681a4edfdb8
lidj22 <36579697+lidj22@users.noreply.github.com>
2024-04-30 12:00:47 -0700

    working helloworld program

---
+++ src/main/java/com/github/lidj22/App.java
@@ -0,0 +1,13 @@
+package com.github.lidj22;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
